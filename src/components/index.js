5f19a3d679e915e5c18979d1f150e1a137a40fc3
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-22 17:04:11 +0100

    ListDetails component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 // export { default as RepoForm } from './RepoForm';
 export { default as SearchBar } from './SearchBar'
 export { default as UserDetails } from './UserDetails'
+// export { default as ListDetails } from './ListDetails'
+
+
+
