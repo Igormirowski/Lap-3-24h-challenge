9628f2db590db660cab0a9ec00edb98eec52e69a
“Igormirowski” <igorpmirowski@gmail.com>
2022-06-22 09:23:09 +0100

    add header and footer

---
+++ src/layout/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './style.css'
+
+const Header = () => {
+    return (
+    <nav>
+       Header
+      
+        
+    </nav>
+    )
+    
+}
+
+export default Header;
